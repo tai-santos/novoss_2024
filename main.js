3f892cd9404cc7e2f2acc8e9d2d7b6d94d1c32fc
tai-santos <159537959+tai-santos@users.noreply.github.com>
2024-09-20 08:31:03 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -19,38 +19,41 @@ const botoes = document.querySelectorAll('.botao');
 
  const contadores = document.querySelectorAll('.contador');
   contadores[0].textContent = "Contagem regressiva";
-  const tempoobjetivo1 = new Date("2024-11-14t00:00:00");
-  const tempoobjetivo2 = new Date("2024-12-05t00:00:00");
-  const tempoobjetivo3 = new Date("2025-01-30t00:00:00");
-  const tempoobjetivo4 = new Date("2025-02-01t00:00:00");
+  const tempoobjetivo1 = new Date("2024-11-18t00:00:00");
+  const tempoobjetivo2 = new Date("2024-12-18t00:00:00");
+  const tempoobjetivo3 = new Date("2025-01-18t00:00:00");
+  const tempoobjetivo4 = new Date("2025-02-18t00:00:00");
 
- const tempos =
- [tempoobjetivo1,tempoobjetivo2,tempoobjetivo3,tempoobjetivo4];
-  contadores[0].textContent = calculaTempo(tempos[0]);
+const tempos =
+ [tempoobjetivo1, tempoobjetivo2, tempoobjetivo3, tempoobjetivo4];
+  
+ contadores[0].textContent = calculaTempo(tempos[0]);
 
   let tempoAtual = new Date();
-   contadores[0].textContent = calculaTempo(tempoobjetivo1);
-   contadores[1].textContent = calculaTempo(tempoobjetivo2);
-   contadores[2].textContent = calculaTempo(tempoobjetivo3);
-   contadores[3].textContent = calculaTempo(tempoobjetivo4);
+//    contadores[0].textContent = calculaTempo(tempoobjetivo1);
+//    contadores[1].textContent = calculaTempo(tempoobjetivo2);
+//    contadores[2].textContent = calculaTempo(tempoobjetivo3);
+//    contadores[3].textContent = calculaTempo(tempoobjetivo4);
    
-   function calculaTempo(tempoobjetivo) {
-    let tempoAtual = new Date();
-    let tempofinal = tempoobjetivo - tempoAtual;
-    let s =  Math.floor(tempofinal/ 1000);
-    let m = Math.floor(s / 60);
-    let h = Math.floor(m/ 60);
-    let d = Math.floor(h/ 24);
 
-    s %=60;
-    m %=60;
-    h %=60;
-
-    return d + " d " + h + " h " + m + " m " + s + " s ";
-   }
 function atualizaCronometro(){
     for (let i=0; i<contadores.length;i++){
-        contadores[i].textContent = calculaTempo(tempo [1]);
+        contadores[i].textContent = calculaTempo(tempos[i]);
     }
 }
 atualizaCronometro();
+
+   function calculaTempo(tempoobjetivo) {
+    let tempoAtual = new Date();
+    let tempofinal = tempoobjetivo - tempoAtual;
+    let segundos =  Math.floor(tempofinal/ 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %=60;
+    minutos %=60;
+    horas %=60;
+
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+   }
